7341999806e0c91bd84e40ebd0ea4c6314287e94
gholiveira29 <g.h.p.o3615@gmail.com>
2023-05-22 14:36:15 -0300

    criando layout e adiconando funcao de deletar

--- src/modules/home/components/todo-list/todo-list.component.ts
+++ src/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskList } from 'src/modules/module/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+
+  taskList: Array<TaskList> = [
+    { task: 'teste', checked: true },
+    { task: 'teste 2', checked: false },
+  ];
+
+  ngOnInit(): void {
+
+  }
+
+  deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  deleteAll() {
+    this.taskList = [];
+  }
 
 }
